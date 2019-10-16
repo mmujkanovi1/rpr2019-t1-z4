d91d6bed44e4045c483b3b10e33c50100562477a
Mujkanovic Mirza <mmujkanovi1@etf.unsa.ba>
2019-10-16 16:43:05 +0200

    2 prepravka

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -3,10 +3,23 @@ package ba.unsa.etf.rpr;
 public class Korpa {
 
 Artikl[] artikl=new Artikl[50];
-    public void dodajArtikl(Artikl a) {
-    }
+    public void dodajArtikl(Artikl a) {int i=0;
+ while(a[i]==null)i++;
+artikl[i]=a;
+
+
+
+}
+public Artikl[] getArtikli(){
+
+return artikl;
+
 
 }
 
 
 
+
+
+
+}
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -9,6 +9,27 @@ Artikl[] artikli = new Artikl[1000];
      while(artikli[i]!=null) i++;
      artikli[i]=a;
     }
+public void izbaciArtiklSaKodom(String kod) {
+
+        int i=0;
+        for(i=0;i<1000;i++) {
+            if(artikli[i].kod=kod) {
+                for(int j=i;j<999;j++) {
+                    artikli[j]=artikli[j+1];
+                }
+            }
+        }
+
+
+}
+
+public Artikl[] getArtikli() {
+        return artikli;
+}
+
+
+
+
 
 
 }
