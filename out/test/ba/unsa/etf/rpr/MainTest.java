1f0ee20f62645a0158829d59bb80062b1c7037b7
Mujkanovic Mirza <mmujkanovi1@etf.unsa.ba>
2019-10-22 23:27:49 +0200

    6.3 prepravka

---
+++ out/test/ba/unsa/etf/rpr/MainTest.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void ispisArtikala() {
+    }
+
+    @Test
+    void main() {
+    }
+}
